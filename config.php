a4e23cab46f6948291bcd0692c83c5846df9a653
Алексей Есев <alekseyesev@live.ru>
2022-08-13 22:15:41 +0300

    Fix config

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ namespace Teplosocial;
 use \Teplosocial\ConfigCommon;
 
 class Config extends ConfigCommon {
-    const MONGO_CONNECTION = 'mongodb://teplosocial-mongo:27017';
+    const MONGO_CONNECTION = 'mongodb://teplosocial-mongo';
     
     const STATS_EXTRA_EMAILS = [
     ];
