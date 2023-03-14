717bb9890ef5588848fa2bc9991716b3a0f787c4
ahaenor <ahaenor@gmail.com>
2023-03-14 20:20:32 +0200

    * config.php for the prod changed.

--- config.php
+++ config.php
@@ -5,10 +5,11 @@ namespace Teplosocial;
 use \Teplosocial\ConfigCommon;
 
 class Config extends ConfigCommon {
-    const MONGO_CONNECTION = 'mongodb://teplosocial-mongo:27017';
-    // teplosocial2.ngo2.ru Mongo config //'mongodb://teplosocial-mongo';
+    const MONGO_CONNECTION = 'mongodb://localhost:27017';
+    // teplosocial2.ngo2.ru Mongo config: //'mongodb://localhost:27017'; or mb 'mongodb://teplosocial-mongo';
     // localhost Mongo config: mongodb://teplosocial-mongo:27017
-    
+    // prod Mongo config: mongodb://localhost:27017
+
     const STATS_EXTRA_EMAILS = [
     ];
 
