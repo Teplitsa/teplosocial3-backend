b712a41c1bae0f7a71789a1929a77abfd8bcbfff
ahaenor <ahaenor@gmail.com>
2023-04-07 19:11:24 +0300

    * Fix for the fatal error on assignment approval. It's possible that it'll also fix the Certificate problems.

--- wp-hooks/learn/module.php
+++ wp-hooks/learn/module.php
@@ -11,7 +11,6 @@ require_once get_template_directory().'/admin/admin-utility-functions.php';
 
 class ModuleHooks {
     public static function handle_module_complete( $data ) {
-        // error_log("handle_module_complete...");
 
         $user = $data['user'];
         $module = $data['course'];
@@ -19,9 +18,9 @@ class ModuleHooks {
         $user_id = $user->ID;
         $course = null;
         $track = null;
-        $completedModule = $module;
-        $completedCourse = null;
-        $completedTrack = null;
+        $completed_module = $module;
+        $completed_course = null;
+        $completed_track = null;
         $uncompleted_module_block = null;
         $uncompleted_course_module = null;
         $uncompleted_track_course = null;
@@ -32,53 +31,56 @@ class ModuleHooks {
         // error_log("module ID: " . $module->ID);
 
         // The Module is completed - update the user activity in the Modules special table:
-        $result = tps_update_user_activity_modules($user_id, $module->ID);
+//        $result = tps_update_user_activity_modules($user_id, $module->ID);
 
         $course = Course::get_by_module($module->ID);
         if($course) {
             // error_log("block course:" . $course->post_name);
             $track = Track::get_by_course($course->ID);
 
-            if($completedModule) {
+            if($completed_module) {
+
                 $uncompleted_course_module = Course::get_first_uncompleted_module($course->ID, $user_id);
-                // error_log("uncompleted_course_module:" . ($uncompleted_course_module ? $uncompleted_course_module->post_name : ""));
-                if(!$uncompleted_course_module) {
+                if( !$uncompleted_course_module ) {
 
                     Course::complete_by_user($course->ID, $user_id);
-                    $completedCourse = $course;
+                    $completed_course = $course;
 
                     // The Course is completed - update the user activity in the Courses special table:
-                    $result = tps_update_user_activity_courses($user_id, $course->ID);
+//                    $result = tps_update_user_activity_courses($user_id, $course->ID);
 
                 }
+
             }
+
         }
 
         if($track) {
             // error_log("block track:" . $track->post_name);
 
-            if($completedCourse) {
+            if($completed_course) {
 
                 $uncompleted_track_course = Track::get_first_uncompleted_course($track->ID, $user_id);
-                // error_log("uncompleted_track_course:" . ($uncompleted_track_course ? $uncompleted_track_course->post_name : ""));
                 if( !$uncompleted_track_course ) {
 
                     Track::complete_by_user($track->ID, $user_id);
-                    $completedTrack = $track;
+                    $completed_track = $track;
 
                     // The Track is completed - update the user activity in the Tracks special table:
-                    $result = tps_update_user_activity_tracks($user_id, $track->ID);
+//                    $result = tps_update_user_activity_tracks($user_id, $track->ID);
 
                 }
+
             }
+
         }
 
-        if($completedCourse) {
-            Certificate::save_certificate($user_id, $completedCourse->post_title, Certificate::CERTIFICATE_TYPE_COURSE, ['course_id' => $completedCourse->ID]);
+        if($completed_course) {
+            Certificate::save_certificate($user_id, $completed_course->post_title, Certificate::CERTIFICATE_TYPE_COURSE, ['course_id' => $completed_course->ID]);
         }
 
-        if($completedTrack) {
-            Certificate::save_certificate($user_id, $completedTrack->post_title, Certificate::CERTIFICATE_TYPE_TRACK, ['track_id' => $completedTrack->ID]);            
+        if($completed_track) {
+            Certificate::save_certificate($user_id, $completed_track->post_title, Certificate::CERTIFICATE_TYPE_TRACK, ['track_id' => $completed_track->ID]);
         }
     }
 }
