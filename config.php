e7448435b349b9dff8cb23e5567d3b3ec6d43460
ahaenor <ahaenor@gmail.com>
2023-06-26 13:02:37 +0300

    * Fix for the target admin email.

--- config.php
+++ config.php
@@ -14,6 +14,7 @@ class Config extends ConfigCommon {
     ];
 
     const NEW_ASSIGNMENT_NOTIFY_EMAILS = [
-        'ahaenor@gmail.com',
+        // 'ahaenor@gmail.com',
+		'maryshally90@gmail.com',
     ];
 }
